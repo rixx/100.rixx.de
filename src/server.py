928faebeb1dfcf85134f99ce72e1f76691e9965b
Tobias Kunze <r@rixx.de>
2021-12-15 14:23:07 +0100

    Add more writing and stuff

--- src/server.py
+++ src/server.py
@@ -1,8 +1,8 @@
-from pathlib import Path
 import json
+from pathlib import Path
 
-from aiohttp import web
 from aiofile import async_open
+from aiohttp import web
 from aiohttp_jinja2 import setup, template
 from jinja2 import FileSystemLoader
 
@@ -12,6 +12,7 @@ DATA_PATH = Path(__file__).parent / "data.json"
 with open(Path(__file__).parent / "config.json") as f:
     CONFIG = json.load(f)
 
+
 async def get_data():
     if not DATA_PATH.exists():
         return {}
@@ -19,14 +20,18 @@ async def get_data():
         content = await f.read()
     return json.loads(content)
 
-async def write_data(key, interval):
+
+async def write_data(key, interval, reason, learned):
     if not DATA_PATH.exists():
         raise Exception("boo")
     data = await get_data()
     data[key]["count"] += interval
+    data[key]["reason"] = reason
+    data[key]["learned"] = learned
     async with async_open(DATA_PATH, "w") as f:
         await f.write(json.dumps(data))
     return data
+
 
 @template("index.html")
 async def home(request, data=None, admin=False):
@@ -43,14 +48,25 @@ async def home_admin(request):
     if request.match_info.get("secret") == CONFIG["secret_key"]:
         admin = True
         data = await request.post()
-        data = await write_data(data["key"], interval=1 if data["direction"] == "up" else -1)
+        interval = 0
+        if data.get("direction") == "up":
+            interval = 1
+        elif data.get("direction") == "down":
+            interval = -1
+        data = await write_data(
+            data["key"],
+            interval=interval,
+            reason=data["reason"],
+            learned=data["learned"],
+        )
     return {"data": data, "admin": admin}
-
 
 
 app = web.Application()
 setup(app, loader=FileSystemLoader(Path(__file__).parent / "templates"))
-app.add_routes([web.get("/", home), web.get("/{secret}", home), web.post("/{secret}", home_admin)])
+app.add_routes(
+    [web.get("/", home), web.get("/{secret}", home), web.post("/{secret}", home_admin)]
+)
 
 if __name__ == "__main__":
     web.run_app(app)
